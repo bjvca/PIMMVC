8779f3bf9e1f5e76b26d5a366ce1079880575d9f
Anusha De <anusha.de@student.kuleuven.be>
2021-05-28 17:23:09 +0200

    code and lyx tables prepared for regressions with seed and stack data (only differences left to be done)

--- papers/perceptions/code/perceptions_seed.R
+++ papers/perceptions/code/perceptions_seed.R
@@ -3,18 +3,337 @@ path <- getwd()
 options(scipen=999)
 path_2 <- strsplit(path, "/papers/perceptions")[[1]]
 
+library(miceadds)
+
 ################# FARMERS:SEED SYSTEMS ######################
 
 ##Farmers' dataset
 farmers_seed <- read.csv(paste(path_2,"/papers/perceptions/data_seed_systems/data/farmer/baseline_farmers.csv", sep = "/"))
+#extracting variables from the baseline data
+farmers_seedsub <- farmers_seed[ , c("Check2.check.maize.q15", "Check2.check.maize.q14",
+                                     "Check2.check.maize.q16", "Check2.check.maize.q17",
+                                     "Check2.check.maize.q8", "farmer_ID")]  
 
+##Farmers' dataset --- long form --- with ratings
+farmers_long <- read.csv(paste(path_2,"/papers/perceptions/data_seed_systems/data/farmer/rating_dyads.csv", sep = "/"))
+
+#merging baseline and long form
+merged_farmers <- merge(farmers_seedsub,farmers_long, by=c("farmer_ID"))
 
 ################# FARMERS : STACK SURVEYS ######################
 
 ##Farmers' dataset
 farmers <- read.csv(paste(path_2,"data/public/farmers.csv", sep = "/"))
 
-farmers1 <- farmers
-farmers1[farmers1=="n/a"] <- NA 
+#############INPUT DEALERS#####################
+#getting dealer ratings from farmers' stack surveys 
 
-farmers[farmers=="999"] <- NA
+##Prepping data
+trans <- c("hh.maize.agro1.q108h","hh.maize.agro1.q108i","hh.maize.agro1.q108j","hh.maize.agro1.q108k","hh.maize.agro1.q108l")
+farmers[trans] <- lapply(farmers[trans], function(x) as.numeric(as.character(x)) )
+trans <- c("hh.maize.agro2.q109h","hh.maize.agro2.q109i","hh.maize.agro2.q109j","hh.maize.agro2.q109k","hh.maize.agro2.q109l")
+farmers[trans] <- lapply(farmers[trans], function(x) as.numeric(as.character(x)) )
+trans <- c("hh.maize.agro3.q111h","hh.maize.agro3.q111i","hh.maize.agro3.q111j","hh.maize.agro3.q111k","hh.maize.agro3.q111l")
+farmers[trans] <- lapply(farmers[trans], function(x) as.numeric(as.character(x)) )
+
+stack1 <- cbind(farmers[c("ID","id.agro1","hh.maize.q25","hh.maize.agro1.q108h","hh.maize.agro1.q108i","hh.maize.agro1.q108j","hh.maize.agro1.q108k","hh.maize.agro1.q108l","hh.maize.q24","hh.maize.q27"
+                          ,"hh.maize.q13","hh.maize.q26")],"Yes")
+names(stack1) <- c("farmerID","id.ratee", "farmer_gender","rating_location","rating_price","rating_quality","rating_stock","rating_reputation","age","education",
+                   "tarmac","marital_status","interaction")
+
+stack2 <- cbind(farmers[c("ID","id.agro2","hh.maize.q25","hh.maize.agro2.q109h","hh.maize.agro2.q109i","hh.maize.agro2.q109j","hh.maize.agro2.q109k","hh.maize.agro2.q109l","hh.maize.agro2.q110","hh.maize.q24","hh.maize.q27"
+                          ,"hh.maize.q13","hh.maize.q26")])
+names(stack2) <- c("farmerID","id.ratee", "farmer_gender","rating_location","rating_price","rating_quality","rating_stock","rating_reputation", "interaction","age","education"
+                   ,"tarmac","marital_status")
+
+stack3 <- cbind(farmers[c("ID","id.agro3","hh.maize.q25","hh.maize.agro3.q111h","hh.maize.agro3.q111i","hh.maize.agro3.q111j","hh.maize.agro3.q111k","hh.maize.agro3.q111l","hh.maize.agro3.q112","hh.maize.q24","hh.maize.q27"
+                          ,"hh.maize.q13","hh.maize.q26")])
+names(stack3) <- c("farmerID","id.ratee", "farmer_gender","rating_location","rating_price","rating_quality","rating_stock","rating_reputation", "interaction","age","education"
+                   ,"tarmac","marital_status")
+
+ratings <-rbind(stack1,stack2,stack3) #all 3 stacks together
+ratings[c("id.ratee","interaction")] <- lapply(ratings[c("id.ratee","interaction")], function(x) as.factor(as.character(x)) )
+
+##Subsetting 
+ratings <- subset(ratings, !is.na(rating_reputation) )
+ratings <- subset(ratings[!apply(ratings == "", 1, any),])
+ratings <- ratings[c(!duplicated(ratings[,1:2])),] #removing duplicates 
+
+which(duplicated(ratings)) #no duplicates
+
+### simple average (Index for overall rating)
+ratings$rating_overall <- rowSums(ratings[c("rating_location","rating_price","rating_quality","rating_stock","rating_reputation")])/5
+summary(ratings$rating_overall)
+
+########### DATA WITH SEED AND STACK SURVEYS ########
+
+#extracting variables from merged seed data
+merged_farmers_seed <- merged_farmers[ , c("Check2.check.maize.q15", "Check2.check.maize.q14",
+                                    "Check2.check.maize.q16", "Check2.check.maize.q17",
+                                    "Check2.check.maize.q8", "farmer_ID", "shop_ID", "bought_at_dealer", 
+                                    "location_rating", "price_rating", "quality_rating", "stock_rating",
+                                    "reputation_rating")] 
+#changing names to match
+names(merged_farmers_seed) <- c("farmer_gender","age","marital_status","education","tarmac","farmerID","id.ratee",
+                                "interaction","rating_location","rating_price", "rating_quality", "rating_stock",
+                                "rating_reputation") 
+
+merged_farmers_seed <- merged_farmers_seed[!(merged_farmers_seed$rating_location=="n/a"),] #removing ratings having n/a
+
+#changing to numeric from character
+merged_farmers_seed$rating_location<-as.numeric(merged_farmers_seed$rating_location)
+merged_farmers_seed$rating_price<-as.numeric(merged_farmers_seed$rating_price)
+merged_farmers_seed$rating_quality<-as.numeric(merged_farmers_seed$rating_quality)
+merged_farmers_seed$rating_stock<-as.numeric(merged_farmers_seed$rating_stock)
+merged_farmers_seed$rating_reputation<-as.numeric(merged_farmers_seed$rating_reputation)
+#getting overall rating measure -- average 
+merged_farmers_seed$rating_overall <- rowSums(merged_farmers_seed[c("rating_location","rating_price",
+                                                                     "rating_quality","rating_stock","rating_reputation")])/5
+### SEED AND STACK DATA TOGETHER
+farmers_seed_stack <-rbind(ratings,merged_farmers_seed)
+#creating dummies for analysis 
+farmers_seed_stack$interaction_yes <- ifelse(farmers_seed_stack$interaction == 'Yes', 1, 0) 
+farmers_seed_stack$gender <- ifelse(farmers_seed_stack$farmer_gender == 'Female', 1, 0) #female farmers
+farmers_seed_stack$educ <- 0
+farmers_seed_stack$educ[farmers_seed_stack$education=="b" | farmers_seed_stack$education=="c" | farmers_seed_stack$education=="d" | farmers_seed_stack$education=="e" | 
+                          farmers_seed_stack$education=="f" |farmers_seed_stack$education=="g" ] <- 1 #educated farmers
+farmers_seed_stack$married <- ifelse(farmers_seed_stack$marital_status == 'a', 1, 0)  #married farmers
+
+farmers_seed_stack[farmers_seed_stack=="999"] <- NA # removing 999
+
+
+####################################################################################################
+####################################################################################################
+
+### CLUSTERED REGRESSIONS - LOOKING AT FARMERS' GENDER ###
+
+#################################################################
+####### DEPENDENT VARIABLE -- RATINGS FROM FARMERS ##############
+#################################################################
+
+################# OVERALL RATING ###########################
+
+#all variables 
+ss1<- lm.cluster(data = farmers_seed_stack, formula = rating_overall ~  gender + age + interaction_yes + educ + tarmac
+                         + married, cluster="id.ratee") 
+s1 <- sqrt(diag(vcov(ss1)))
+ss_res1<-ss1$lm_res
+
+################# LOCATION RATING ###########################
+
+#all variables 
+ss2<- lm.cluster(data = farmers_seed_stack, formula = rating_location ~  gender + age + interaction_yes + educ + tarmac
+                 + married, cluster="id.ratee") 
+s2 <- sqrt(diag(vcov(ss2)))
+ss_res2<-ss2$lm_res
+
+
+################# QUALITY RATING ###########################
+
+#all variables 
+ss3<- lm.cluster(data = farmers_seed_stack, formula = rating_quality ~  gender + age + interaction_yes + educ + tarmac
+                 + married, cluster="id.ratee") 
+s3 <- sqrt(diag(vcov(ss3)))
+ss_res3<-ss3$lm_res
+
+################# PRICE RATING ###########################
+
+#all variables 
+ss4<- lm.cluster(data = farmers_seed_stack, formula = rating_price ~  gender + age + interaction_yes + educ + tarmac
+                 + married, cluster="id.ratee") 
+s4 <- sqrt(diag(vcov(ss4)))
+ss_res4<-ss4$lm_res
+
+################# STOCK RATING ###########################
+#all variables
+ss5<- lm.cluster(data = farmers_seed_stack, formula = rating_stock ~  gender + age + interaction_yes + educ + tarmac
+                 + married, cluster="id.ratee") 
+s5 <- sqrt(diag(vcov(ss5)))
+ss_res5<-ss5$lm_res
+
+################# REPUTATION RATING ###########################
+#all variables
+ss6<- lm.cluster(data = farmers_seed_stack, formula = rating_reputation ~  gender + age + interaction_yes + educ + tarmac
+                 + married, cluster="id.ratee") 
+s6 <- sqrt(diag(vcov(ss6)))
+ss_res6<-ss6$lm_res
+
+
+#######################################################################
+#######################################################################
+
+################# DEALERS:SEED SYSTEMS ######################
+dealers_seed <- read.csv(paste(path_2,"/papers/perceptions/data_seed_systems/data/input_dealer/baseline_dealer.csv", sep = "/"))
+
+#extracting variables from the baseline data
+dealers_sub <- dealers_seed[ , c("maize.owner.agree.q99", "maize.owner.agree.q100",
+                                     "maize.owner.agree.q101", "maize.owner.agree.q102",
+                                     "maize.owner.agree.q103", "shop_ID","maize.owner.agree.age",
+                                 "maize.owner.agree.gender", "maize.owner.agree.educ")] 
+names(dealers_sub) <- c("self_rating_location", "self_rating_price","self_rating_quality", "self_rating_stock",
+                        "self_rating_reputation", "id.ratee","age_dealer", "gender_dealer", "educ_dealer") 
+
+################# DEALERS:STACK SURVEYS ######################
+## Getting Dealers' data
+dealers <- read.csv(paste(path_2,"data/public/agro_input_dealers.csv", sep = "/"))
+
+dealers_sub1 <- dealers[ , c("hh.maize.q6g", "hh.maize.q7","hh.maize.q9","hh.maize.q79",
+                             "hh.maize.q80", "hh.maize.q81", "hh.maize.q82", "hh.maize.q83",
+                              "id.agro")]  
+
+names(dealers_sub1) <- c("age_dealer", "gender_dealer", "educ_dealer","self_rating_location", "self_rating_price","self_rating_quality", "self_rating_stock",
+                        "self_rating_reputation", "id.ratee") 
+
+dealers_ss <-rbind(dealers_sub, dealers_sub1) ### putting both datasets together
+
+##Index for overall rating by dealers for themselves 
+dealers_ss$ratee_rating_overall <- rowSums(dealers_ss[c("self_rating_location", "self_rating_price","self_rating_quality", "self_rating_stock",
+                                                        "self_rating_reputation")])/5
+summary(dealers_ss$ratee_rating_overall)
+
+## creating dummies
+dealers_ss$dealer_fem <- ifelse(dealers_ss$gender_dealer == 'Female', 1, 0) #female dealers
+dealers_ss$education_dealer <- 0
+dealers_ss$education_dealer[dealers_ss$educ_dealer=="b" | dealers_ss$educ_dealer=="c" | dealers_ss$educ_dealer=="d" | dealers_ss$educ_dealer=="e" | 
+                         dealers_ss$educ_dealer=="f" |dealers_ss$educ_dealer=="g" ] <- 1 #educated dealers 
+
+dealers_ss[dealers_ss=="999"]<- NA
+
+
+####### DEPENDENT VARIABLE -- RATINGS FROM DEALERS -- DATASET ONLY HAVING ALL THE DEALERS ##############
+#######################################################################################################
+
+################# OVERALL RATING #################################################################
+
+r1<- lm(data = dealers_ss, formula = ratee_rating_overall ~ dealer_fem + age_dealer + education_dealer) 
+se_dealer1 <- sqrt(diag(vcov(r1)))
+
+################# LOCATION RATING ###########################
+
+r2<- lm(data = dealers_ss, formula = self_rating_location ~ dealer_fem + age_dealer + education_dealer) 
+se_dealer2 <- sqrt(diag(vcov(r2)))
+
+################# QUALITY RATING ###########################
+
+r3<- lm(data = dealers_ss, formula = self_rating_quality ~ dealer_fem + age_dealer + education_dealer) 
+se_dealer3 <- sqrt(diag(vcov(r3)))
+
+################# PRICE RATING ###########################
+
+r4<- lm(data = dealers_ss, formula = self_rating_price ~ dealer_fem + age_dealer + education_dealer) 
+se_dealer4 <- sqrt(diag(vcov(r4)))
+
+################# STOCK RATING ###########################
+
+r5<- lm(data = dealers_ss, formula = self_rating_stock ~ dealer_fem + age_dealer + education_dealer) 
+se_dealer5 <- sqrt(diag(vcov(r5)))
+
+################# REPUTATION RATING ###########################
+
+r6<- lm(data = dealers_ss, formula = self_rating_reputation ~ dealer_fem + age_dealer + education_dealer) 
+se_dealer6 <- sqrt(diag(vcov(r6)))
+
+################################################################################################################
+################################################################################################################
+
+
+################# CREATING DATASET WITH BOTH FARMER AND DEALER DETAILS ###########################
+##################################################################################################
+#getting variables needed from dealer dataset
+d <- dealers_ss[ , c("id.ratee", "age_dealer","dealer_fem","education_dealer")]  
+
+#merging farmers and dealers data by dealer id
+m <- merge(farmers_seed_stack,d, by=c("id.ratee"))
+
+###########################################################################
+### CLUSTERED REGRESSIONS - LOOKING AT BOTH FARMERS' AND DEALERS' GENDER ###
+#################################################################
+####### DEPENDENT VARIABLE -- RATINGS FROM FARMERS ##############
+#################################################################
+
+################# OVERALL RATING ###########################
+
+#all variables 
+fd1<- lm.cluster(data = m, formula = rating_overall ~  gender + dealer_fem + age + interaction_yes + educ + tarmac
+                         + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd1 <- sqrt(diag(vcov(fd1)))
+res_fd1<-fd1$lm_res
+
+##Interaction between sex of farmer and dealer
+fd2<- lm.cluster(data = m, formula = rating_overall ~  gender*dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd2 <- sqrt(diag(vcov(fd2)))
+res_fd2<-fd2$lm_res
+
+################# LOCATION RATING ###########################
+
+#all variables 
+fd3<- lm.cluster(data = m, formula = rating_location ~  gender + dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd3 <- sqrt(diag(vcov(fd3)))
+res_fd3<-fd3$lm_res
+
+##Interaction between sex of farmer and dealer
+fd4<- lm.cluster(data = m, formula = rating_location ~  gender*dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd4 <- sqrt(diag(vcov(fd4)))
+res_fd4<-fd4$lm_res
+
+################# QUALITY RATING ###########################
+
+#all variables 
+fd5<- lm.cluster(data = m, formula = rating_quality ~  gender + dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd5 <- sqrt(diag(vcov(fd5)))
+res_fd5<-fd5$lm_res
+
+##Interaction between sex of farmer and dealer
+fd6<- lm.cluster(data = m, formula = rating_quality ~  gender*dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd6 <- sqrt(diag(vcov(fd6)))
+res_fd6<-fd6$lm_res
+
+################# PRICE RATING ###########################
+
+#all variables 
+fd7<- lm.cluster(data = m, formula = rating_price ~  gender + dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd7 <- sqrt(diag(vcov(fd7)))
+res_fd7<-fd7$lm_res
+
+##Interaction between sex of farmer and dealer
+fd8<- lm.cluster(data = m, formula = rating_price ~  gender*dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd8 <- sqrt(diag(vcov(fd8)))
+res_fd8<-fd8$lm_res
+
+################# STOCK RATING ###########################
+
+#all variables 
+fd9<- lm.cluster(data = m, formula = rating_stock ~  gender + dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd9 <- sqrt(diag(vcov(fd9)))
+res_fd9<-fd9$lm_res
+
+##Interaction between sex of farmer and dealer
+fd10<- lm.cluster(data = m, formula = rating_stock ~  gender*dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd10 <- sqrt(diag(vcov(fd10)))
+res_fd10<-fd10$lm_res
+
+################# REPUTATION RATING ###########################
+
+#all variables 
+fd11<- lm.cluster(data = m, formula = rating_reputation ~  gender + dealer_fem + age + interaction_yes + educ + tarmac
+                 + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd11 <- sqrt(diag(vcov(fd11)))
+res_fd11<-fd11$lm_res
+
+##Interaction between sex of farmer and dealer
+fd12<- lm.cluster(data = m, formula = rating_reputation ~  gender*dealer_fem + age + interaction_yes + educ + tarmac
+                  + married + age_dealer + education_dealer, cluster="id.ratee") 
+se_fd12 <- sqrt(diag(vcov(fd12)))
+res_fd12<-fd12$lm_res
+
+#########################################################################################################################
+#########################################################################################################################
