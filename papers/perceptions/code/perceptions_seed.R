0223b47affa324f2bd07d8159cbe041073924005
Anusha De <u0133866@kuleuven.be>
2022-03-09 14:50:43 +0100

    FE at farmer level without controls, table also added for seed ratings

--- papers/perceptions/code/perceptions_seed.R
+++ papers/perceptions/code/perceptions_seed.R
@@ -43,6 +43,7 @@ rating_dyads <- rbind(rating_dyads, rating_dyads_midline)
 #keep only those that have same gender in baseline and endline
 
 rating_dyads <- subset(rating_dyads,shop_ID  %in% to_select)
+rat<-rating_dyads  #to be used for FE
 
 # convert to numbers and aggregate scores at agro-input dealer level for between regression
 
@@ -240,3 +241,154 @@ s7<- rbind(c((format(round(sum((lm(overall_rating~gender,data=rating_dyads))$coe
              (format(round(nobs(lm(reputation_rating~gender,data=rating_dyads)),digits=3),nsmall=0)))
 )
 
+
+
+############## FIXED EFFECTS 
+
+#merge to get gender 
+rat <- merge(rat, baseline_dealer[c("shop_ID","maize.owner.agree.gender")],  by.x="shop_ID", by.y="shop_ID", all.x=T)
+#changing variable name 
+names(rat)[names(rat) == "maize.owner.agree.gender"] <- "gender"
+
+rat[rat=="n/a"]<- NA 
+rat[rat=="98"]<- NA 
+rat[rat=="999"]<- NA 
+
+rat$dealer_ID <- 1:nrow(rat) #creating dealer ID, otherwise there will be couple duplicates of farmer and dealer IDs and plm won't run 
+
+#creating numeric ratings 
+rat$general_rating<-as.numeric(rat$general_rating)
+rat$location_rating<-as.numeric(rat$location_rating)
+rat$price_rating<-as.numeric(rat$price_rating)
+rat$quality_rating<-as.numeric(rat$stock_rating)
+rat$stock_rating<-as.numeric(rat$stock_rating)
+rat$reputation_rating<-as.numeric(rat$reputation_rating)
+
+rat$seed_quality_general_rating<-as.numeric(rat$seed_quality_general_rating)
+rat$seed_yield_rating<-as.numeric(rat$seed_yield_rating)
+rat$seed_drought_rating<-as.numeric(rat$seed_drought_rating)
+rat$seed_disease_rating<-as.numeric(rat$seed_disease_rating)
+rat$seed_maturing_rating<-as.numeric(rat$seed_maturing_rating)
+rat$seed_germinate_rating<-as.numeric(rat$seed_germinate_rating)
+
+rat$overall_rating <-  rowMeans(rat[c("general_rating","location_rating","price_rating","quality_rating","stock_rating","reputation_rating")],na.rm=T) #overall dealer rating 
+rat$score  <-  rowMeans(rat[c("seed_quality_general_rating","seed_yield_rating","seed_drought_rating","seed_disease_rating","seed_maturing_rating","seed_germinate_rating")],na.rm=T)    #overall seed rating 
+
+#FE at farmer level --- without controls 
+#dealer ratings 
+
+plm(overall_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(overall_rating~gender+farmer_ID, data = rat))
+
+plm(general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(general_rating~gender+farmer_ID, data = rat))
+
+plm(location_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(location_rating~gender+farmer_ID, data = rat))
+
+plm(price_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(price_rating~gender+farmer_ID, data = rat))
+
+plm(quality_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(quality_rating~gender+farmer_ID, data = rat))
+
+plm(stock_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(stock_rating~gender+farmer_ID, data = rat))
+
+plm(reputation_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(reputation_rating~gender+farmer_ID, data = rat))
+
+#seed ratings 
+
+plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(score~gender+farmer_ID, data = rat))
+
+plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(seed_quality_general_rating~gender+farmer_ID, data = rat))
+
+plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(seed_yield_rating~gender+farmer_ID, data = rat))
+
+plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(seed_drought_rating~gender+farmer_ID, data = rat))
+
+plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(seed_disease_rating~gender+farmer_ID, data = rat))
+
+plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(seed_maturing_rating~gender+farmer_ID, data = rat))
+
+plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")
+summary(lm(seed_germinate_rating~gender+farmer_ID, data = rat))
+
+
+
+fe1<- rbind( c((format(round(mean(fixef(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")))[1],digits=3),nsmall=0)),
+               (format(round(mean(fixef(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")))[1],digits=3),nsmall=0)),
+               (format(round(mean(fixef(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")))[1],digits=3),nsmall=0)),
+               (format(round(mean(fixef(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")))[1],digits=3),nsmall=0)),
+               (format(round(mean(fixef(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")))[1],digits=3),nsmall=0)),
+               (format(round(mean(fixef(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")))[1],digits=3),nsmall=0)),
+               (format(round(mean(fixef(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")))[1],digits=3),nsmall=0))),
+             c((format(round(sum((plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$coefficients[1]),digits=3),nsmall=0)),
+               (format(round(sum((plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$coefficients[1]),digits=3),nsmall=0)),
+               (format(round(sum((plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$coefficients[1]),digits=3),nsmall=0)),
+               (format(round(sum((plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$coefficients[1]),digits=3),nsmall=0)),
+               (format(round(sum((plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$coefficients[1]),digits=3),nsmall=0)),
+               (format(round(sum((plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$coefficients[1]),digits=3),nsmall=0)),
+               (format(round(sum((plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$coefficients[1]),digits=3),nsmall=0))),
+             c((format(round((coeftest(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+[1,2],digits=3),nsmall=0)),
+(format(round((coeftest(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+              [1,2],digits=3),nsmall=0)),
+(format(round((coeftest(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+              [1,2],digits=3),nsmall=0)),
+(format(round((coeftest(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+              [1,2],digits=3),nsmall=0)),
+(format(round((coeftest(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+              [1,2],digits=3),nsmall=0)),
+(format(round((coeftest(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+              [1,2],digits=3),nsmall=0)),
+(format(round((coeftest(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+              [1,2],digits=3),nsmall=0))),
+
+c((format(round((coeftest(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+                [1,4],digits=3),nsmall=0)),
+  (format(round((coeftest(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+                [1,4],digits=3),nsmall=0)),
+  (format(round((coeftest(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+                [1,4],digits=3),nsmall=0)),
+  (format(round((coeftest(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+                [1,4],digits=3),nsmall=0)),
+  (format(round((coeftest(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+                [1,4],digits=3),nsmall=0)),
+  (format(round((coeftest(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+                [1,4],digits=3),nsmall=0)),
+  (format(round((coeftest(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), vcovHC(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"), type = "HC0", cluster = "time")))
+                [1,4],digits=3),nsmall=0))),
+             
+             
+             c((format(round(summary(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[1],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[1],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[1],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[1],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[1],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[1],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[1],digits=3),nsmall=0))),
+           
+  c((format(round(summary(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[2],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[2],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[2],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[2],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[2],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[2],digits=3),nsmall=0)),
+               (format(round(summary(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within"))$r.squared[2],digits=3),nsmall=0))),
+           
+  c((format(round(nobs(plm(score~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")),digits=3),nsmall=0)),
+               (format(round(nobs(plm(seed_quality_general_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")),digits=3),nsmall=0)),
+               (format(round(nobs(plm(seed_yield_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")),digits=3),nsmall=0)),
+               (format(round(nobs(plm(seed_drought_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")),digits=3),nsmall=0)),
+               (format(round(nobs(plm(seed_disease_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")),digits=3),nsmall=0)),
+               (format(round(nobs(plm(seed_maturing_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")),digits=3),nsmall=0)),
+               (format(round(nobs(plm(seed_germinate_rating~gender, data = rat, index=c("farmer_ID","dealer_ID"), model="within")),digits=3),nsmall=0)))
+)
